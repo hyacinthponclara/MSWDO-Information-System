0ecfeb481da526e2a1eb27f91ec18bfcb25d9993
Trishadel Macalino <trishadelmacalino15@gmail.com>
2026-05-04 16:50:58 +0800

    changed to php file, linked to sidebar

--- casestudy.php
+++ casestudy.php
@@ -1,3 +1,8 @@
+<?php
+require 'auth.php';
+requireRole(['Social Worker']);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -397,7 +402,7 @@
 <body class="bg-slate2 min-h-screen flex">
 
     <!-- ═══════════════════ SIDEBAR ═══════════════════ -->
-
+    <?php include 'sidebar.php'; ?>
     <!-- ═══════════════════ MAIN ═══════════════════ -->
     <div class="ml-56 flex-1 flex flex-col min-h-screen">
 
