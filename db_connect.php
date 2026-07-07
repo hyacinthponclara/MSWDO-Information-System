58cd346f7505a8bfd26bc1dc80ac9d8638740dd8
Trishadel Macalino <trishadelmacalino15@gmail.com>
2026-07-08 00:05:32 +0800

    connect to railway

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,17 @@
 <?php
-$host = 'localhost';
-$dbname = 'CAPSTONE_DRAFT';
-$db_user = 'root';
-$db_pass = ''; 
+// Pull the credentials securely from Railway's environment variables
+$host = getenv('mysql.railway.internal');
+$port = getenv('19886');
+$dbname = getenv('railway'); 
+$db_user = getenv('root');
+$db_pass = getenv('zvDiFeHtRYptIXBTEIwIpKpPxTJTUCri'); 
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $db_user, $db_pass);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     error_log($e->getMessage());
     header('Location: index.html?error=server');
     exit;
 }
+?>
--- login.php
+++ login.php
@@ -14,13 +14,15 @@ if (empty($username) || empty($password)) {
     exit;
 }
 
-$host    = 'localhost';
-$dbname  = 'CAPSTONE_DRAFT';
-$db_user = 'root';
-$db_pass = '';  
+$host = getenv('mysql.railway.internal');
+$port = getenv('19886');
+$dbname = getenv('railway'); 
+$db_user = getenv('root');
+$db_pass = getenv('zvDiFeHtRYptIXBTEIwIpKpPxTJTUCri'); 
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $db_user, $db_pass);
+    // Added port=$port here!
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $db_user, $db_pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     error_log($e->getMessage());
