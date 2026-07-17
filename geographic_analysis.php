7831b272b62fb71163eccde41e7eb6ebf1112a19
lexzyaanam <manaay.alexanne01@gmail.com>
2026-07-17 19:34:45 +0000

    negros

--- geographic_analysis.php
+++ geographic_analysis.php
@@ -116,7 +116,7 @@ $programData['women_child'] = $women;
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Geographic Analysis – MSWDO San Enrique</title>
+    <title>Geographic Analysis – MSWDO San Enrique, Negros Occidental</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link
         href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500;9..40,600&display=swap"
